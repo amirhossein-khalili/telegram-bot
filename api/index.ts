0d6e20cb7df6c3b8a063a7cf573dc0b7038b9d1a
amir <amir1378khalili@gmail.com>
2024-09-20 19:06:38 +0330

    initial commit

---
+++ api/index.ts
@@ -0,0 +1,23 @@
+require('../src/index');
+
+import { webhookCallback } from 'grammy';
+import bot from '../src/core/bot';
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+async function handler(req, res) {
+    await runMiddleware(req, res, webhookCallback(bot, 'https'));
+}
+
+export default handler;
